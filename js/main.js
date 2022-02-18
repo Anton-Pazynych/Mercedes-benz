129d73206d1725ba63b596badcddeb4e19e8297b
Anton Pazynych <97320787+Anton-Pazynych@users.noreply.github.com>
2022-02-18 05:28:18 +0100

    add gallery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(document).ready(function(){
+  $('.slider__body').slick({
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<img class="slider__arrow-l" src="/img/slider_arrow_2.png" alt="arrow">',
+    nextArrow: '<img class="slider__arrow-r" src="/img/slider_arrow_1.png" alt="arrow">',
+    
+  });
 });
