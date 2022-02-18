e4d11d195de159948d2c6cafceebf4736915934a
Anton Pazynych <97320787+Anton-Pazynych@users.noreply.github.com>
2022-02-18 05:32:50 +0100

    img fix gallery

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     dots: false,
     slidesToShow: 4,
     slidesToScroll: 4,
-    prevArrow: '<img class="slider__arrow-l" src="/img/slider_arrow_2.png" alt="arrow">',
-    nextArrow: '<img class="slider__arrow-r" src="/img/slider_arrow_1.png" alt="arrow">',
+    prevArrow: '<img class="slider__arrow-l" src="../img/slider_arrow_2.png" alt="arrow">',
+    nextArrow: '<img class="slider__arrow-r" src="../img/slider_arrow_1.png" alt="arrow">',
     
   });
 });
